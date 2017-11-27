67113a9c83bf60f4b92edaf4a0a3cdde23090926
abdelrahman-t <talaataboudakika@std.sehir.edu.tr>
2017-11-27 19:44:29 +0300

    cleanup

---
+++ multirotor/src/TestUtilities.py
@@ -0,0 +1,23 @@
+import unittest
+from Utilities import *
+from Common import *
+
+
+class TestUtilities(unittest.TestCase):
+    df = pd.read_csv('unit_test.csv')
+
+    def test_rotation(self):
+        pass
+
+    def test_quaternionConversion(self):
+        pass
+
+    def test_integrateTrajectoryVelocityBody(self):
+        pass
+
+    def test_integrateTrajectoryAccelerationBody(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
